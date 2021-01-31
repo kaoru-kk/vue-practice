8d2540aa76fbe52b4aa56b8b63b223580523c347
19980410 <kkaoru1998@gmail.com>
2021-01-31 22:41:10 +0900

    classの動的切替

--- udemy/index.js
+++ udemy/index.js
@@ -1,29 +1,17 @@
 new Vue({
   el: '#app',
   data: {
-    counter: 0,
-    otherCounter: 0
-    // date同士は参照できない
-    // NG:　lessThanThree: this.count
+    isActive: true,
+    color: 'red',
+    bg: 'bg-blue'
   },
-  // 算出プロパティ
-  // dataプロパティのように扱う
   computed: {
-    lessThanThreeComputed(){
-      console.log('computed');
-      return this.counter > 3  ? 'up 3' : 'down 3'
+    classObject () {
+      return {
+        red: this.isActive,
+        'bg-blue': !this.isActive
+      }
     }
-  },
-  // watch: 特定のデータが変わったときに使用する
-  // Computedでカバーできないときに使う（非同期処理とか
-  watch: {
-    counter: function(){
-      // settimeout内でthisを使えないのでここで定義
-      const vm = this;
-      setTimeout(function() {
-        vm.counter = 0
-      }, 3000)
-    }
-
-  } 
+    
+  }
 })
