8770eeb7e3891cc554fb3b0e4602bed2806c61f3
19980410 <kkaoru1998@gmail.com>
2021-01-31 18:48:06 +0900

    なぜMethodsをViewsで実行させることはまずいのか（computed, methodsの違い

--- udemy/index.js
+++ udemy/index.js
@@ -1,14 +1,23 @@
 new Vue({
   el: '#app',
   data: {
-    counter: 0
+    counter: 0,
+    otherCounter: 0
     // date同士は参照できない
     // NG:　lessThanThree: this.count
   },
   // 算出プロパティ
   // dataプロパティのように扱う
   computed: {
-    lessThanThree(){
+    lessThanThreeComputed(){
+      console.log('computed');
+      return this.counter > 3  ? 'up 3' : 'down 3'
+    }
+  },
+  methods: {
+    // 関係ないクリックイベントが発火した時も再描画されるので、問題
+    lessThanThreeMethod(){
+      console.log('methods');
       return this.counter > 3  ? 'up 3' : 'down 3'
     }
   }
