5ee60b0c27c858809dc4ebb37f22af27f7002c4c
19980410 <kkaoru1998@gmail.com>
2021-01-31 17:25:07 +0900

    add:

---
+++ udemy/index.js
@@ -0,0 +1,36 @@
+new Vue({
+	el: '#app',
+  data: {
+    message: 'hello lworld',
+    number: 2,
+    disabled: true,
+    html: '<h1>h1 desu </h1>',
+    url: 'https://google.com',
+    number: 1,
+    attribute: 'href',
+    hrefObject: {
+      href: 'https://google.com',
+      id: 222
+    },
+    x: 0,
+    y: 0
+  },
+  methods: {
+    reverseMessage: function(){
+      this.message  = this.message.split('').reverse().join('')
+    },
+    say: function(a='222'){
+      this.message = 'fefefffefe'
+      return this.message;
+    },
+    countUp(){
+      this.number += 1
+    },
+    // イベントオブジェクト
+    changeMousePosition(text, event){
+      this.x = event.clientX;
+      this.y = event.clientY;
+      console.log(text);
+    }
+  }
+})
