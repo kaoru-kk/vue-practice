f42aba8e6b4b7ccd03163ce8d155c27733ae20b4
19980410 <kkaoru1998@gmail.com>
2021-01-31 18:06:23 +0900

    add: vuex state

--- vuex/index.js
+++ vuex/index.js
@@ -1,8 +1,33 @@
+// storeの作成
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    two: 2
+  },
+  mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--,
+  }
+})
+
 const Counter = {
   template: `<div> {{ count }} </div>`,
   computed: {
     count (){
-      return store.state.count
+      return this.$store.state.count + this.$store.state.two
     }
   }
 }
+
+
+const app = new Vue({
+  el: '#app',
+  // "store" オプションで指定されたストアは、全ての子コンポーネントに注入されます
+  store,
+  components: { Counter },
+  template: `
+    <div class="app">
+      <Counter></Counter>
+    </div>
+  `
+})
